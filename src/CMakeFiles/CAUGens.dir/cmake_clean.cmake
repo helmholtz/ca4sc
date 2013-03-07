328c258b20346833dcf16f20aae27e5bb655d2b1
yota morimoto <yotamorimoto@gmail.com>
2013-03-07 18:11:45 +0100

    cmakelists update

---
+++ src/CMakeFiles/CAUGens.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CAUGens.dir/CAUGens.cpp.o"
+  "CAUGens.pdb"
+  "CAUGens.scx"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CAUGens.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
